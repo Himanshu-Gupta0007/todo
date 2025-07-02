e69ac17262f577935b6b27c4741014bec7e2606a
guptahimu90@gmail.com <guptahimu90@.com>
2025-07-02 11:53:48 +0530

    Fix: Switched to HashRouter to resolve Vercel 404

--- rtk-todo/src/main.jsx
+++ rtk-todo/src/main.jsx
@@ -1,18 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {Provider} from 'react-redux'
-import { store } from './Store.js'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { store } from './Store.js';
+import { HashRouter } from 'react-router-dom'; // ✅ Add this line
 
 createRoot(document.getElementById('root')).render(
-
-
-
-<Provider store={store} >
-<StrictMode>
-
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <HashRouter> {/* ✅ Wrap App inside this */}
+        <App />
+      </HashRouter>
+    </StrictMode>
   </Provider>
-)
+);
